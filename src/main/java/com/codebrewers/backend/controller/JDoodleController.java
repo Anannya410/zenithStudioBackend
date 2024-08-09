6de8c9d0652f171ca44eb5f38de318d4d3a77ae5
Annanya410 <anannya04hiteshi@gmail.com>
2024-08-09 19:24:33 +0530

    Added functionality to compile code

---
+++ src/main/java/com/codebrewers/backend/controller/JDoodleController.java
@@ -0,0 +1,20 @@
+package com.codebrewers.backend.controller;
+
+import com.codebrewers.backend.dao.JDoodleRequest;
+import com.codebrewers.backend.dao.JDoodleResponse;
+import com.codebrewers.backend.service.JDoodleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class JDoodleController {
+
+    @Autowired
+    private JDoodleService jdoodleService;
+
+    @PostMapping("/execute")
+    public JDoodleResponse executeScript(@RequestBody JDoodleRequest jdoodleRequest) {
+        return jdoodleService.executeScript(jdoodleRequest);
+    }
+}
